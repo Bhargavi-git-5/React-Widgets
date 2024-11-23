849cc97fca42425c8e58e7ec3847a104f4eac011
unknown <bhargavi.n.t45@gmail.com>
2024-11-23 08:12:07 +0530

    Modified RBAC-UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div>
+      <h4 style={{textAlign:'center',textDecoration:'underline'}}>RBAC-Application</h4>
       <nav>
         <Link to="/users">User Management</Link> | 
         <Link to="/roles">Role Management</Link>
